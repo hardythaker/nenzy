362f026c7c573cf03264e07f505931b8e83c27d5
Hardik Thaker <hardik.thaker1994@gmail.com>
2024-03-09 02:45:26 +0530

    Changed DB name to Dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,14 +13,15 @@ import { JobTitleModule } from './job-title/job-title.module';
     ConfigModule.forRoot({
       validationSchema: Joi.object({
         DATABASE_URI: Joi.string().required(), //DATABASE_URI env variable is required to start the APP.
-      })
+      }),
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (config: ConfigService) => ({
         uri: config.get<string>('DATABASE_URI'), // Loaded from .ENV
-      })
+        dbName: config.get<string>('DATABASE_NAME'), // Loaded from .ENV
+      }),
     }),
     LoggerModule.forRoot({
       pinoHttp: {
@@ -30,7 +31,7 @@ import { JobTitleModule } from './job-title/job-title.module';
             singleLine: true,
           },
         },
-      }
+      },
     }),
     JobModule,
     JobTitleModule

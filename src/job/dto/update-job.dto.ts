47533fb5d9ad52e9b7e93435fdc4ea49e17a9e03
Hardik Thaker <hardik.thaker1994@gmail.com>
2024-03-23 19:54:02 +0530

    Added new DTO prop

--- src/job/dto/update-job.dto.ts
+++ src/job/dto/update-job.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateJobDto } from './create-job.dto';
+import { IsBoolean } from 'class-validator';
 
-export class UpdateJobDto extends PartialType(CreateJobDto) {}
+export class UpdateJobDto extends PartialType(CreateJobDto) {
+  @IsBoolean()
+  isPublic?: boolean;
+}

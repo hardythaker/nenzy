a3552649b12676eb638d58ec82abec360047b2c6
Hardik Thaker <hardik.thaker1994@gmail.com>
2024-03-31 05:18:59 +0530

    Swagger documentation

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -3,6 +3,7 @@ import { CompanyService } from './company.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { Company } from './entities/company.entity';
 
 @ApiTags('Company')
 @Controller('company')
@@ -15,22 +16,25 @@ export class CompanyController {
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Company[]> {
     return this.companyService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<Company | null> {
     return this.companyService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateCompanyDto: UpdateCompanyDto,
+  ): Promise<Company | null> {
     return this.companyService.update(id, updateCompanyDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: string): Promise<Company | null> {
     return this.companyService.remove(id);
   }
 }

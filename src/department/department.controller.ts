0cb459589cc4a24b4a34fd0367ea241eeeaf8a1b
Hardik Thaker <hardik.thaker1994@gmail.com>
2024-03-31 05:23:20 +0530

    Swagger documentation

--- src/department/department.controller.ts
+++ src/department/department.controller.ts
@@ -3,6 +3,7 @@ import { DepartmentService } from './department.service';
 import { CreateDepartmentDto } from './dto/create-department.dto';
 import { UpdateDepartmentDto } from './dto/update-department.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { Department } from './entity/department.entity';
 
 @ApiTags('Department')
 @Controller('department')
@@ -10,17 +11,19 @@ export class DepartmentController {
   constructor(private readonly departmentService: DepartmentService) {}
 
   @Post()
-  create(@Body() createDepartmentDto: CreateDepartmentDto) {
+  create(
+    @Body() createDepartmentDto: CreateDepartmentDto,
+  ): Promise<Department> {
     return this.departmentService.create(createDepartmentDto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Department[]> {
     return this.departmentService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<Department | null> {
     return this.departmentService.findOne(id);
   }
 
@@ -28,12 +31,12 @@ export class DepartmentController {
   update(
     @Param('id') id: string,
     @Body() updateDepartmentDto: UpdateDepartmentDto,
-  ) {
+  ): Promise<Department | null> {
     return this.departmentService.update(id, updateDepartmentDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: string): Promise<Department | null> {
     return this.departmentService.remove(id);
   }
 }
